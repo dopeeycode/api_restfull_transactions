6d246be2c444981bcfed4d4014ff63eeb5eab5ce
Italo <italodopeey@gmail.com>
2023-07-08 22:37:13 -0300

    first commit

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,15 @@
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify'
+
+export async function checkSessionIdExists(
+  request: FastifyRequest,
+  reply: FastifyReply,
+  done: HookHandlerDoneFunction,
+) {
+  const sessionId = request.cookies.sessionId
+
+  if (!sessionId) {
+    return reply.status(401).send({ message: 'Unauthorized' })
+  }
+
+  done()
+}
